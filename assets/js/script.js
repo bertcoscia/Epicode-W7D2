34ca5639d66fb20a81526878f21a6d1d8fca7f39
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-06-11 15:49:15 +0200

    completed first exer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 const form = document.querySelector("form");
 const nameForm = document.getElementById("name");
-const saveBtn = document.getElementById("saveBtn");
 const deleteBtn = document.getElementById("deleteBtn");
 const title = document.querySelector("h1");
 
@@ -9,18 +8,29 @@ const createName = (name) => {
   localStorage.setItem("nameInStorage", name);
 };
 
+const welcomeBack = (name) => {
+  title.innerHTML = `Welcome back ${name} &#128516;`;
+};
+
 form.onsubmit = function (event) {
   event.preventDefault();
   createName(nameForm.value);
   form.reset();
 };
 
+const clearStorage = () => {
+  localStorage.clear();
+  title.innerHTML = "Write your name &#128516;";
+};
+
 window.addEventListener("DOMContentLoaded", () => {
   const nameInStorage = localStorage.getItem("nameInStorage");
 
   if (nameInStorage) {
-    createName(nameInStorage);
+    welcomeBack(nameInStorage);
   } else {
     title.innerHTML = "Write your name &#128516;";
   }
 });
+
+deleteBtn.addEventListener("click", clearStorage);
